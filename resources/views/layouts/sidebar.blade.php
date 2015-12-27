f1b51420a91a55fdaec61334fbaf95fbd10815d5
Evan Smith <esincork@gmail.com>
2015-12-27 23:01:38 +0000

    Add home link to sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,11 +6,15 @@
             	<img src="{{URL::to('/')}}/images/logo.png" />
             </a>
         </li>
+        <li id="home"><a href="{{URL::to('/')}}" class="waves-effect waves-red">Home</a></li>
         <li id="mysql"><a href="{{ URL::route('manage/mysql') }}" class="waves-effect waves-red">MySQL Databases</a></li>
         <li id="account"><a href="{{ URL::route('manage/account') }}" class="waves-effect waves-red">Account</a></li>
         <li id="wordpress"><a href="{{ URL::route('manage/wordpress') }}" class="waves-effect waves-red">Install Wordpress</a></li>
         <li id="backups"><a href="{{ URL::route('manage/backups') }}" class="waves-effect waves-red">Backups</a></li>
+        
         <li id="ssh"><a href="{{ URL::route('static/ssh') }}" class="waves-effect waves-red">How To Login</a></li>
+        <li id="irc"><a href="{{ URL::route('static/irc') }}" class="waves-effect waves-red">How To IRC</a></li>
+
         <li id="logout"><a href="{{ URL::route('logout') }}" class="waves-effect waves-red">Logout</a></li>
         {{-- <li class="no-padding">
           <ul class="collapsible collapsible-accordion">
